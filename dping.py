436df938f01653c5e8538b88f50fb4c7d2d4fc1d
dantate <daniel.tate@gmail.com>
2021-11-17 18:48:06 -0600

    Improved version

--- dping.py
+++ dping.py
@@ -5,6 +5,10 @@ import subprocess
 import time
 import argparse
 import datetime
+import re
+from itertools import islice
+from subprocess import Popen, PIPE
+
  
 sys.tracebacklimit = 0
 
@@ -19,6 +23,7 @@ action="store_true")
 parser.add_argument("-B", "--heartbeat", help= "Heartbeat", action="store_true")
 parser.add_argument("-s", "--sleep", help= "Sleep for this long between pings",
         default=1)
+parser.print_help()
 args = parser.parse_args()
 hostname = args.Host
 if args.timestamp:
@@ -27,21 +32,43 @@ if args.timestamp:
     
 
     with open(os.devnull, 'wb') as devnull:
-        response = subprocess.call(['/usr/sbin/ping', '-c1', '-W1', hostname ],
-                stdout=devnull, stderr=subprocess.STDOUT)
-    if response == 0:
-        while 1:
-            ct = datetime.datetime.now()
-            print (ct,  "\033[1;32;40m" u"\u2588" "\033[0;37;40m", "Up", end='\n',
-                flush=True )
-            time.sleep(args.sleep)
-    elif response == 1:
-        while 1:
-            ct = datetime.datetime.now()
-            print (ct, "\033[1;31;40m" u"\u2588" "\033[0;37;40m", "Down", end='\n', flush=True )
-            time.sleep(args.sleep)
-    else:
-        print (" Error code ", response )
+        response = Popen(['/usr/sbin/ping', '-c1', '-W1', hostname ],
+                stdout=subprocess.PIPE, stderr=devnull)
+        output=response.communicate()
+        test_string = str(output)
+        #print ("DEBUG: Output:", test_string)
+        my_pattern='ttl'
+        res=test_string.rstrip('\n\n').split(" ")
+        ttl=re.sub(r'\\n\\n\',','', res[19])
+        ttl_up=re.sub(r'\\n\\n\',','', res[11])
+        #print ("DEBUG: res:", res) 
+        #print("DEBUG: TTL:", ttl)
+
+        #nprint ("DEBUG: Response:", response)
+        # Establish pattern for getting return code of ping..
+        pattern = 'returncode: 1'
+        test_string2 = str(response)
+        result = re.search(pattern, test_string2)
+        count = 0 
+        if result:
+            #print("DEBUG: Pattern Found")
+            while 1:
+
+                ct = datetime.datetime.now()
+                ctf = ct.strftime("%Y-%m-%d %H:%M:%S")
+                print (ctf, "\033[1;31;40m" u"\u2588" "\033[0;37;40m", "Down | ttl", ttl, "count:", count, end='\n', flush=True )
+                time.sleep(args.sleep)
+                count = count+1
+        else:
+            #print("DEBUG: in else loop")
+            while 1:
+                ct = datetime.datetime.now()
+                ctf = ct.strftime("%Y-%m-%d %H:%M:%S")
+                print (ctf,  "\033[1;32;40m" u"\u2588" "\033[0;37;40m", "Up |",ttl_up, "count:", count, end='\n', flush=True )
+                time.sleep(args.sleep)
+                count = count+1
+    #else:
+     #   print (" Error code ", response )
 
 if args.blocks:
     print ("Blocks mode selected")
